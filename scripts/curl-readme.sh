0b22a6bc734ee63a92f3d8ca3032100a8d9f6b3e
Daniel Snider <danielsnider12@gmail.com>
2020-05-16 14:16:12 -0700

    Helper script to get the most up to date markdown files

---
+++ scripts/curl-readme.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# These READMEs are used in the website as pieces of the portfolio:
+curl https://raw.githubusercontent.com/danielsnider/cell-bio/master/README.md -o ../src/static/cell-bio-readme.md
+curl https://raw.githubusercontent.com/danielsnider/ros-rover/master/README.md -o ../src/static/ros-rover-readme.md
