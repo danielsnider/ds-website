499298533b5c425fd88cb39f0feb55d69ceeb9d0
Daniel Snider <danielsnider12@gmail.com>
2020-05-16 14:15:51 -0700

    add more content

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
-// import React, { useState } from 'react'
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from "react-dom"
 import { Document, Page, pdfjs } from 'react-pdf';
 import useScrollInfo from 'react-element-scroll-hook'
 
-import objectTrackingPdf from './images/object-tracking.pdf';
 import './styles/main.css'
 
 
@@ -26,6 +24,12 @@ const section = {
         title: 'Cat Photo 2 (dog)',
         src: 'placeholder-dog.cbd3e9e1.jpg',
       },
+      rosRover: {
+        id: 'ros-rover',
+        title: 'Mars Rover',
+        src: 'https://github.com/danielsnider/ros-rover/blob/master/README.md',
+        description: 'How to build a one-armed, self-driving, University Rover Challenge robot with ROS',
+      },
       dogPhoto: {
         id: 'dog-photo',
         title: 'Dog Photo',
@@ -43,11 +47,6 @@ const section = {
         title: 'Object Tracking',
         src: 'object-tracking.6ad48331.jpg',
       },
-      objectTrackingPdf: {
-        id: 'object-tracking',
-        title: 'Object Tracking',
-        file: objectTrackingPdf,
-      }
     }
   },
   fullStack: {
@@ -55,6 +54,11 @@ const section = {
     title: 'Full Stack',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dictum ultrices libero, id venenatis quam facilisis et. Mauris ultrices volutpat commodo. Proin at fringilla lectus. Pellentesque aliquet mi ac nunc finibus sagittis. Nulla non finibus velit.',
     items: {
+      deepScatter: {
+        id: 'deep-scatter',
+        title: 'A.I. Web App',
+        src: 'deepscatter.0b40bbf5.jpg',
+      },
       breqwatr: {
         id: 'breqwatr',
         title: 'Breqwatr Private Cloud Appliance',
@@ -71,8 +75,6 @@ const section = {
 
 class Main extends React.Component {
   render() {
-    const d = new Date()
-    console.log('reload at ' + d.toLocaleTimeString())
     return (
       <div className='main'>
         <div className='header'>
@@ -104,14 +106,15 @@ class Main extends React.Component {
             <ImageItem {...section.robotics.items.dogPhoto}/>
             <ImageItem {...section.robotics.items.dogPhoto}/>
           </Section>
-          <Section {...section.robotics}>
+          {/* <Section {...section.robotics}>
             <ImageItem {...section.robotics.items.catPhoto}/>
             <ImageItem {...section.robotics.items.catPhoto2}/>
             <ImageItem {...section.robotics.items.catPhoto}/>
             <ImageItem {...section.robotics.items.catPhoto}/>
-          </Section>
+          </Section> */}
           <Section {...section.fullStack}>
             <ImageItem {...section.fullStack.items.breqwatr}/>
+            <ImageItem {...section.fullStack.items.deepScatter}/>
             <ImageItem {...section.fullStack.items.matkit}/>
           </Section>
         </div>
@@ -136,6 +139,15 @@ class Main extends React.Component {
 
 function Section(props) {
   const [scrollInfo, setRef, ref] = useScrollInfo()
+
+  const sectionElem = document.getElementById('computer-vision')
+  const sectionRect = sectionElem ? sectionElem.getBoundingClientRect() : null
+  // console.log('sectionRect: ', sectionRect)
+
+  const itemElem = document.getElementById('object-tracking')
+  const rect = itemElem ? itemElem.getBoundingClientRect() : null
+  // console.log('rect: ', rect)
+
   return (
     <div id={props.id} className='portfolio-section'>
       <div className='portfolio-section-left'>
@@ -179,7 +191,48 @@ class ImageItem extends React.Component {
   }
 }
 
+class IframeItem extends React.Component {
+  /*
+    1. To embed any webpage into an iframe these scripts are needed in index.html
+    <script src="https://unpkg.com/@ungap/custom-elements-builtin"></script>
+    <script type="module" src="https://unpkg.com/x-frame-bypass"></script>
+    From: https://github.com/niutech/x-frame-bypass
+
+    2. Add item data to a content section
+      rosRover: {
+        id: 'ros-rover',
+        title: 'Mars Rover',
+        src: 'https://github.com/danielsnider/ros-rover/blob/master/README.md',
+        description: 'How to build a one-armed, self-driving, University Rover Challenge robot with ROS',
+      },
+
+    3. Then use this component:
+    <IframeItem {...section.robotics.items.rosRover}/>
+  */
+  render() {
+    return (
+      <div className='image-item'>
+        <iframe width='800' height='700' is="x-frame-bypass" src={this.props.src}></iframe>
+      </div>
+    )
+  }
+}
+
 class PdfItem extends React.Component {
+  /*
+    1. Do the import the file
+    import objectTrackingPdf from './static/object-tracking.pdf';
+
+    2. Add item data to a content section
+    objectTrackingPdf: {
+      id: 'object-tracking',
+      title: 'Object Tracking',
+      file: objectTrackingPdf,
+    }
+
+    3. Then use this component:
+    <PdfItem {...section.computerVision.items.objectTrackingPdf}/>
+  */
   render() {
     return (
       <div className='image-item' id={this.props.id} className='image-img'>
@@ -194,15 +247,6 @@ class PdfItem extends React.Component {
   }
 }
 
-class GithubReadmeItem extends React.Component {
-  render() {
-    return (
-      <div>
-        <img id={this.props.id} src={this.props.src}/>
-      </div>
-    )
-  }
-}
 
 var mountNode = document.getElementById("app")
 ReactDOM.render(<Main name="Daniel" />, mountNode)
