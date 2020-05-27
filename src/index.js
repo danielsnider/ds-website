c0ebbee9e79318db52a8f690fe3cec6ebf8512ff
Daniel Snider <danielsnider12@gmail.com>
2020-05-26 19:52:45 -0700

    trying the images folder

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ColoredScrollbars from './ColoredScrollbars';
 
 import './styles/main.css'
 // // import cellBioMarkdown from './static/cell-bio-readme.md'
-import rosRoverMarkdown from './static/ros-rover-readme.md'
+import rosRoverMarkdown from './images/ros-rover-readme.md' // https://danielsnider.ca/ros-rover-readme.b1516c05.html
 import imageArchiveImage50 from './images/image-archive-UI_50.jpg'
 import imageArchiveImage20 from './images/image-archive-UI_20.jpg'
 import breqwatrCardImage from './images/breqwatr-card-50.jpg'
