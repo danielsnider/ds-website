511805f2520937c4df246fa9209c0acd5e82b055
Daniel Snider <danielsnider12@gmail.com>
2020-06-01 17:12:07 -0700

    cleanup about

--- src/index.js
+++ src/index.js
@@ -39,18 +39,8 @@ function Header(props) {
         {/* <Index/> */}
         <div className='about' id="content">
           <div className='about-sentance'>Hello and Welcome!</div>
-          {/* <div className='about-sentance'>
-            My name is Daniel and <span className='about-emphasis'>I <span className='about-emphasis2'>❤</span> automation.</span>
-          </div>
           <div className='about-sentance'>
-            I want to help people do more.
-          </div>
-          <div className='about-sentance'>
-            I want to help people do more.
-          </div> */}
-          <div className='about-sentance'>
-            {/* My name is Daniel and <span className='about-emphasis'>I automate for people with <span className='about-emphasis2'>❤</span>.</span> */}
-            My name is Daniel and <span className='about-emphasis'>I automate for people with 
+            My name is Daniel and <span className='about-emphasis'>I automate for people with
             <svg style={{paddingLeft: '5px'}} version="1.1" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 28 28">
               <title>heart</title>
               <path d="M14 26c-0.25 0-0.5-0.094-0.688-0.281l-9.75-9.406c-0.125-0.109-3.563-3.25-3.563-7 0-4.578 2.797-7.313 7.469-7.313 2.734 0 5.297 2.156 6.531 3.375 1.234-1.219 3.797-3.375 6.531-3.375 4.672 0 7.469 2.734 7.469 7.313 0 3.75-3.437 6.891-3.578 7.031l-9.734 9.375c-0.187 0.187-0.438 0.281-0.688 0.281z" fill="#f1aaae"></path>
